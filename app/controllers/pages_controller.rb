f479fd56299a0ae85407542df97a739b0768fe01
telo <diogo_telo@hotmail.com>
2020-06-02 17:52:21 +0100

    1st changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home , :bank, :transportation]
 
   def home
   end
+
+  def bank
+  end
+
+  def transportation
+  end
+
 end
