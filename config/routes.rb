321279bb97984a2240b79775f3c31ceefdb7ec15
telo <diogo_telo@hotmail.com>
2021-01-11 16:59:28 +0000

    add a page when the payment could not be processed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'bank', to: 'pages#bank'
+  get 'bank_denied', to: 'pages#bank_denied'
   get 'transportation', to: 'pages#transportation'
 end
